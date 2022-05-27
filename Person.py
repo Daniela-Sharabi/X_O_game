af737e7c90a2ca4d988e7f0491698503ea05240a
hitmachut <hitmachut@students.local>
2022-05-27 11:15:26 +0300

    second

--- Person.py
+++ Person.py
@@ -1,7 +1,8 @@
 class Person:
     def __init__(self, name, last_name):
-        self.name = name
-        self.last_name = last_name
+      :'/'
 
     def get_name(self):
+        print('hi')
         return self.name + self.last_name
+
